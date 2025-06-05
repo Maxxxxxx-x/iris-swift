f9ca9fa2f5b1b9568bce120f9076c00ea2efb053
Anson-YH <yeungyinho@gmail.com>
2025-06-06 05:15:39 +0800

    feat: added smtp client & send mail function

--- services/smtp/smtp.gp.go
+++ services/smtp/smtp.gp.go
@@ -7,14 +7,57 @@ import (
 	"github.com/emersion/go-smtp"
 )
 
-func NewClient(cfg config.SMTPConfig) error {
+type SmtpClient interface {
+	Send(to string, body string) error
+	Close() error
+}
+
+type smtpClient struct {
+	client *smtp.Client
+	config config.SMTPConfig
+}
+
+func NewClient(cfg config.SMTPConfig) (SmtpClient, error) {
 	addr := fmt.Sprintf("%s:%s", cfg.Host, cfg.Port)
 	client, err := smtp.Dial(addr)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	auth := sasl.NewPlainClient("", cfg.Base_Sender_Email, cfg.Password)
 	if err := client.Auth(auth); err != nil {
-		
+		return nil, err
 	}
+
+	smtpclient := &smtpClient{
+		client: client,
+	}
+
+	return smtpclient, nil
+}
+
+func (s *smtpClient) Send(to string, body string) error {
+	if err := s.client.Mail(s.config.Base_Sender_Email, nil); err != nil {
+		return err
+	}
+	if err := s.client.Rcpt(to, nil); err != nil {
+		return err
+	}
+	w, err := s.client.Data()
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	if _, err := w.Write([]byte(body)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *smtpClient) Close() error {
+	if err := s.client.Quit(); err != nil {
+		return err
+	}
+	return nil
 }
