19dc21003e18adffad8a75a82141dc41c6ae6fae
Maxxxxxx-x <maxxng1028@gmail.com>
2025-06-06 07:41:42 +0800

    feat: Token structs and claims

--- services/jwt_token/model.go
+++ services/jwt_token/model.go
@@ -1,16 +1,55 @@
 package token
 
 import (
-	"github.com/Maxxxxxx-x/iris-swift/config"
+	"time"
+
 	"github.com/cristalhq/jwt/v5"
+	"github.com/rs/zerolog"
 )
 
 type ITokenManager interface {
-
+	GenerateAccessToken(userId, role string) (*jwt.Token, string, error)
+	GenerateRefreshToken(userId, accessTokenId string) (*jwt.Token, error)
+	GenerateVerifyEmailToken(email string) (*jwt.Token, error)
+	GenerateResetPasswordToken(email string) (*jwt.Token, error)
+	VerifyAccessToken(rawToken string) (*jwt.Token, error)
+	VerifyRefreshToken(rawToken string) (*jwt.Token, error)
+	VerifyVerifyEmailToken(rawToken string) (*jwt.Token, error)
+	VerifyResetPasswordToken(rawToken string) (*jwt.Token, error)
 }
 
 type TokenManager struct {
-	config config.JWTConfig
-	accessTokenSigner jwt.Signer
-	refreshTokenSigner jwt.Signer
+	access        SignerVerifier
+	refresh       SignerVerifier
+	verifyEmail   SignerVerifier
+	resetPassword SignerVerifier
+	logger        zerolog.Logger
+}
+
+type SignerVerifier struct {
+	Signer   jwt.Signer
+	Verifier jwt.Verifier
+	TTL      time.Duration
+}
+
+type AccessTokenClaims struct {
+	UserId      string `json:"user_id"`
+	AccountType string `json:"account_type"`
+	jwt.RegisteredClaims
+}
+
+type RefreshTokenClaims struct {
+	AccessTokenId string `json:"ati"`
+	UserId        string `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+type VerifyEmailTokenClaims struct {
+	Email string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+type ResetPasswordTokenClaims struct {
+	Email string `json:"email"`
+	jwt.RegisteredClaims
 }
